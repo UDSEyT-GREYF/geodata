dfbc6caf061f66eeb429db4197f2511ada59b87b
UDSEyT-GREYF <daetye@gmail.com>
2026-07-03 13:26:12 -0300

    Update impacto-socioeconomico.js

--- js/impacto-socioeconomico.js
+++ js/impacto-socioeconomico.js
@@ -999,9 +999,10 @@ drawEmploymentPeopleInline(
     iconCount: getEmploymentIconCount(indirect, maxCategory, 10, 3),
     iconX: 385,
     lines: [
-      "Originados en la cadena de proveedores de bienes y servicios",
-      "vinculados a las actividades directas. No existirían sin",
-      "la infraestructura aeroportuaria y la conectividad aérea",
+      "Originados en la economía del área de influencia aeroportuaria",
+      "como parte de la cadena de proveedores de bienes y servicios.", 
+      "de las actividades directas. Estos empleos no tendrían lugar",
+      "sin la infraestructura aeroportuaria y la conectividad aérea",
       "Ej.: insumos, mercaderías, publicidad y",
       " logística vinculadas al aeropuerto"
     ]
