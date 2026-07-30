85c906938c502c3ab31b568398ef44ddaf1263b1
UDSEyT-GREYF <daetye@gmail.com>
2026-07-30 10:56:03 -0300

    Update impacto-covid-aviacion-argentina-tablas.js

--- js/impacto-covid-aviacion-argentina-tablas.js
+++ js/impacto-covid-aviacion-argentina-tablas.js
@@ -993,7 +993,7 @@ if (intConclusions) {
       ${
         intFallBack.length
           ? `casos recuperados en ${reportConfig.lastAnnualYear} que muestran retroceso frente al primer semestre de ${reportConfig.h1BaseYear}: ${escapeHtml(intFallBack.join("; "))}.`
-          : "sin retrocesos en el primer semestre de 2026 entre los aeropuertos recuperados en 2025."
+          : "sin casos de retrocesos en el primer semestre de 2026 entre los aeropuertos recuperados en 2025."
       }
     </p>
   `;
