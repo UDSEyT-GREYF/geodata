8210bf1965957916c5e7625c3b0fc07edf869441
UDSEyT-GREYF <daetye@gmail.com>
2026-08-11 11:24:23 -0300

    Update impacto-covid-aviacion-argentina-tablas.js

--- js/impacto-covid-aviacion-argentina-tablas.js
+++ js/impacto-covid-aviacion-argentina-tablas.js
@@ -35,7 +35,7 @@
     const MARGINAL_MAX_ANNUAL_PAX = 1000;
     const MARGINAL_LABEL = "Volumen marginal";
     
-    const CAB_ROWS_FIRST_PAGE = 10;
+    const CAB_ROWS_FIRST_PAGE = 9;
     const CAB_ROWS_CONTINUATION_SNA = 24;
   
     const INT_ROWS_FIRST_PAGE_SNA = 11;
