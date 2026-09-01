964674c4e41fc1be2c90d0612855df390f6522a6
UDSEyT-GREYF <daetye@gmail.com>
2026-09-01 11:02:38 -0300

    Update editor-efluentes-config.js

--- js/editor-efluentes-config.js
+++ js/editor-efluentes-config.js
@@ -3,10 +3,9 @@
 // NUNCA colocar la service_role key en un archivo que se publique en GitHub Pages.
 
 window.SIGA_EDITOR_CONFIG = {
-  supabaseUrl: "PEGAR_AQUI_PROJECT_URL",
-  supabaseAnonKey: "PEGAR_AQUI_PUBLISHABLE_O_ANON_KEY",
+  supabaseUrl: "https://arsmoxqyosomorakzasa.supabase.co",
+  supabaseAnonKey: "sb_publishable_rzlL03Rek-M5XUbw-NwXcA_CBNjaKvy",
 
-  // Fuentes ya utilizadas por SIGA en el repositorio geodata.
   airportsSource: "fuentes/Datos_aeropuertos.geojson",
   airportPolygonsSource: "fuentes/poligonos_aeropuertos.geojson"
 };
