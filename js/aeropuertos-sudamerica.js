533a224b341d9489a3103302a03d73bde32dd567
UDSEyT-GREYF <daetye@gmail.com>
2026-09-09 16:45:43 -0300

    Update aeropuertos-sudamerica.js

--- js/aeropuertos-sudamerica.js
+++ js/aeropuertos-sudamerica.js
@@ -3,8 +3,8 @@
 
 const DATA_URLS = {
   airports: "data/sudamerica/aeropuertos_sudamerica.geojson",
-  operationsAnnual: "data/sudamerica/datos_operativos_aeropuertos_anual.csv",
-  operationsMonthly: "data/sudamerica/datos_operativos_aeropuertos_mensual.csv",
+  operationsAnnual: "data/sudamerica/operativo/datos_operativos_aeropuertos_anual.csv",
+  operationsMonthly: "data/sudamerica/operativo/datos_operativos_aeropuertos_mensual.csv",
   administrative: "data/sudamerica/divisiones_administrativas_sudamerica.geojson",
   countries: "data/sudamerica/limites_paises_sudamerica.geojson"
 };
